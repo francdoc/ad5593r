687d52c720ca47c91c809ad4c7af951641617d1e
FranCDoc <fchiesadoc@gmail.com>
2024-01-28 00:31:13 -0300

    adding more functions

--- ad5593r_i2c.c
+++ ad5593r_i2c.c
@@ -70,6 +70,7 @@ struct AD5593R
     float _ADC_max;
     float _DAC_max;
 
+    bool _int_vref_enabled;
     bool _ADC_2x_mode;
     bool _DAC_2x_mode;
 };
@@ -91,6 +92,7 @@ void AD5593R_init(struct AD5593R *device, int a0)
     device->_Vref = -1;
     device->_ADC_2x_mode = 0;
     device->_DAC_2x_mode = 0;
+    device->_int_vref_enabled = 0;
     device->_ADC_max = -1;
     device->_DAC_max = -1;
 
@@ -117,19 +119,21 @@ void AD5593R_init(struct AD5593R *device, int a0)
     }
 
     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico) -> hw pins 6 and 7.
-    i2c_init(&i2c, 100*1000);
+    i2c_init(&i2c, 100 * 1000);
     gpio_set_function(SCL, GPIO_FUNC_I2C);
     gpio_set_function(SDA, GPIO_FUNC_I2C);
-    
+
     gpio_pull_up(SCL);
     gpio_pull_up(SDA);
-    
+
     // Make the I2C pins available to picotool
-    // bi_decl(bi_2pins_with_func(SDA, SCL, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(SDA, SCL, GPIO_FUNC_I2C));
+
+    printf("ad5593r initialized.");
 }
 
 void AD5593R_enable_internal_Vref(struct AD5593R *device)
-{   
+{
     // Enable selected device for writing
     device->_Vref = 2.5;
     device->_ADC_max = device->_Vref;
@@ -155,6 +159,10 @@ void AD5593R_enable_internal_Vref(struct AD5593R *device)
     {
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
+
+    device->_int_vref_enabled = 1;
+
+    printf("ad5593r internal vref enabled.");
 }
 
 void AD5593R_disable_internal_Vref(struct AD5593R *device)
@@ -174,18 +182,19 @@ void AD5593R_disable_internal_Vref(struct AD5593R *device)
         device->_PCR_msbs = device->_PCR_msbs ^ 0x02;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_POWER_REF_CTRL;
-    buffer[1] = device->_PCR_msbs;
-    buffer[2] = device->_PCR_lsbs;
+    uint8_t buf[] = {_ADAC_POWER_REF_CTRL, device->_PCR_msbs, device->_PCR_lsbs};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
     {
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
+
+    device->_int_vref_enabled = 1;
+
+    printf("ad5593r internal vref disabled.");
 }
 
 void AD5593R_set_ADC_max_2x_Vref(struct AD5593R *device)
@@ -202,12 +211,9 @@ void AD5593R_set_ADC_max_2x_Vref(struct AD5593R *device)
         device->_GPRC_lsbs = device->_GPRC_lsbs ^ 0x20;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_GP_CONTROL;
-    buffer[1] = device->_GPRC_msbs;
-    buffer[2] = device->_GPRC_lsbs;
+    uint8_t buf[] = {_ADAC_GP_CONTROL, device->_GPRC_msbs, device->_GPRC_lsbs};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
@@ -215,6 +221,8 @@ void AD5593R_set_ADC_max_2x_Vref(struct AD5593R *device)
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
     device->_ADC_2x_mode = 1;
+
+    printf("ad5593r adc max voltage = 2xVref set.");
 }
 
 void AD5593R_set_ADC_max_1x_Vref(struct AD5593R *device)
@@ -231,18 +239,18 @@ void AD5593R_set_ADC_max_1x_Vref(struct AD5593R *device)
         device->_GPRC_lsbs = device->_GPRC_lsbs ^ 0x20;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_GP_CONTROL;
-    buffer[1] = device->_GPRC_msbs;
-    buffer[2] = device->_GPRC_lsbs;
+    uint8_t buf[] = {_ADAC_GP_CONTROL, device->_GPRC_msbs, device->_GPRC_lsbs};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
     {
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
+
+    printf("ad5593r adc max voltage = 1xVref set.");
+
     device->_ADC_2x_mode = 0;
 }
 
@@ -260,12 +268,9 @@ void AD5593R_set_DAC_max_2x_Vref(struct AD5593R *device)
         device->_GPRC_lsbs = device->_GPRC_lsbs ^ 0x10;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_GP_CONTROL;
-    buffer[1] = device->_GPRC_msbs;
-    buffer[2] = device->_GPRC_lsbs;
+    uint8_t buf[] = {_ADAC_GP_CONTROL, device->_GPRC_msbs, device->_GPRC_lsbs};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
@@ -273,6 +278,8 @@ void AD5593R_set_DAC_max_2x_Vref(struct AD5593R *device)
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
     device->_DAC_2x_mode = 1;
+
+    printf("ad5593r dac max voltage = 2xVref set.");
 }
 
 void AD5593R_set_DAC_max_1x_Vref(struct AD5593R *device)
@@ -289,12 +296,9 @@ void AD5593R_set_DAC_max_1x_Vref(struct AD5593R *device)
         device->_GPRC_lsbs = device->_GPRC_lsbs ^ 0x10;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_GP_CONTROL;
-    buffer[1] = device->_GPRC_msbs;
-    buffer[2] = device->_GPRC_lsbs;
+    uint8_t buf[] = {_ADAC_GP_CONTROL, device->_GPRC_msbs, device->_GPRC_lsbs};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
@@ -302,6 +306,8 @@ void AD5593R_set_DAC_max_1x_Vref(struct AD5593R *device)
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
     device->_DAC_2x_mode = 0;
+
+    printf("ad5593r adc max voltage = 1xVref set.");
 }
 
 void AD5593R_set_Vref(struct AD5593R *device, float Vref)
@@ -324,6 +330,8 @@ void AD5593R_set_Vref(struct AD5593R *device, float Vref)
     {
         device->_DAC_max = 2 * Vref;
     }
+
+    printf("ad5593r vref set.");
 }
 
 void AD5593R_configure_DAC(struct AD5593R *device, int channel)
@@ -341,18 +349,46 @@ void AD5593R_configure_DAC(struct AD5593R *device, int channel)
         device->_DAC_config = device->_DAC_config ^ channel_byte;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_DAC_CONFIG;
-    buffer[1] = 0x00;
-    buffer[2] = device->_DAC_config;
+    uint8_t buf[] = {_ADAC_DAC_CONFIG, 0x00, device->_DAC_config};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
     {
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
+
+    printf("ad55593r is configured as a DAC.");
+}
+
+void AD5593R_configure_DACs(struct AD5593R *device, bool *channels, int num_channels)
+{
+    device->_DAC_config = 0;
+    if (device->_a0 > -1)
+    {
+        gpio_put(device->_a0, 0); // Set the GPIO pin low
+    }
+
+    for (size_t i = 0; i < num_channels; i++)
+    {
+        device->config.DACs[i] = channels[i]; // configure each DAC channel according to *channels
+        if (channels[i])
+        {
+            device->_DAC_config |= (1 << i);
+        }
+    }
+    uint8_t buf[] = {_ADAC_DAC_CONFIG, 0x00, device->_DAC_config};
+
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
+
+    // Disable selected device for writing
+    if (device->_a0 > -1)
+    {
+        gpio_put(device->_a0, 1); // Set the GPIO pin high
+    }
+
+    printf("ad55593r is configured as a DAC.");
 }
 
 void AD5593R_write_DAC(struct AD5593R *device, int channel, float voltage)
@@ -360,17 +396,17 @@ void AD5593R_write_DAC(struct AD5593R *device, int channel, float voltage)
     // error checking
     if (device->config.DACs[channel] == 0)
     {
-        printf("Channel %d is not configured as a DAC\n", channel);
+        printf("Channel %d is not configured as a DAC.\n", channel);
         return;
     }
     if (device->_DAC_max == -1)
     {
-        printf("Vref or DAC max has not been set\n");
+        printf("Vref or DAC max has not been set.\n");
         return;
     }
     if (voltage > device->_DAC_max)
     {
-        printf("Voltage is greater than DAC max\n");
+        printf("Voltage is greater than DAC max.\n");
         return;
     }
 
@@ -390,7 +426,7 @@ void AD5593R_write_DAC(struct AD5593R *device, int channel, float voltage)
     data_msbs = (0b00110000 | (channel << 4)) | data_msbs; // 0b00110000 is the DAC write command
 
     uint8_t buffer[3];
-    buffer[0] = _ADAC_DAC_WRITE | channel;
+    buffer[0] = _ADAC_DAC_WRITE | channel; // equal to 0b00010001 in binary, which is equivalent to 17 in decimal notation
     buffer[1] = data_msbs;
     buffer[2] = data_lsbs;
 
@@ -401,6 +437,8 @@ void AD5593R_write_DAC(struct AD5593R *device, int channel, float voltage)
     {
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
+
+    printf("ad5593r write dac ok.");
 }
 
 float AD5593R_configure_ADC(struct AD5593R *device, int channel)
@@ -418,12 +456,9 @@ float AD5593R_configure_ADC(struct AD5593R *device, int channel)
         device->_ADC_config = device->_ADC_config ^ channel_byte;
     }
 
-    uint8_t buffer[3];
-    buffer[0] = _ADAC_ADC_CONFIG;
-    buffer[1] = 0x00;
-    buffer[2] = device->_ADC_config;
+    uint8_t buf[] = {_ADAC_ADC_CONFIG, 0x00, device->_ADC_config};
 
-    i2c_write_blocking(&i2c, device->_i2c_address, buffer, 3, false);
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
 
     // Disable selected device for writing
     if (device->_a0 > -1)
@@ -431,22 +466,44 @@ float AD5593R_configure_ADC(struct AD5593R *device, int channel)
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
 
-    printf("Channel %d is now configured as an ADC\n", channel);
+    printf("Channel %d is now configured as an ADC.\n", channel);
 }
 
 void AD5593R_configure_ADCs(struct AD5593R *device, int *channels, int num_channels)
 {
+
+    device->_ADC_config = 0;
+    if (device->_a0 > -1)
+    {
+        gpio_put(device->_a0, 0); // Set the GPIO pin low
+    }
+
     for (size_t i = 0; i < num_channels; i++)
     {
-        AD5593R_configure_ADC(device, channels[i]);
+        device->config.ADCs[i] = channels[i];
+        if (channels[i])
+        {
+            device->_ADC_config |= (1 << i);
+        }
     }
+    uint8_t buf[] = {_ADAC_ADC_CONFIG, 0x00, device->_ADC_config};
+
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, 3, false);
+
+    // Disable selected device for writing
+    if (device->_a0 > -1)
+    {
+        gpio_put(device->_a0, 1); // Set the GPIO pin high
+    }
+
+    printf("ad55593r is configured as an ADC.");
 }
 
 float AD5593R_read_ADC(struct AD5593R *device, uint8_t channel)
 {
     if (device->config.ADCs[channel] == 0)
     {
-        printf("ERROR! Channel %d is not an ADC\n", channel);
+        printf("ERROR! Channel %d is not an ADC.\n", channel);
         return -1.0f;
     }
 
@@ -468,10 +525,10 @@ float AD5593R_read_ADC(struct AD5593R *device, uint8_t channel)
     uint8_t adc_read_cmd[] = {_ADAC_ADC_READ};
     i2c_write_blocking(&i2c, device->_i2c_address, adc_read_cmd, sizeof(adc_read_cmd), false);
 
-    uint16_t data_bits = 0;
-
     uint8_t data[2];
     i2c_read_blocking(&i2c, device->_i2c_address, data, sizeof(data), false);
+
+    uint16_t data_bits = 0;
     data_bits = (data[0] & 0x0F) << 8 | data[1]; // 0x0F is to mask the 4 most signifigant bits
 
     if (device->_a0 > -1)
@@ -507,6 +564,83 @@ float *AD5593R_read_ADCs(struct AD5593R *device)
     }
 
     return adc_values;
+}
+
+uint16_t AD5593R_read_ADC_bits(struct AD5593R *device, uint8_t channel)
+{
+    uint8_t data_channel = (1 << channel);
+    uint8_t adc_sequence_cmd[] = {_ADAC_ADC_SEQUENCE, 0x02, data_channel};
+    i2c_write_blocking(&i2c, device->_i2c_address, adc_sequence_cmd, sizeof(adc_sequence_cmd), false);
+
+    uint8_t adc_read_cmd[] = {_ADAC_ADC_READ};
+    i2c_write_blocking(&i2c, device->_i2c_address, adc_read_cmd, sizeof(adc_read_cmd), false);
+
+    uint8_t data[2];
+    i2c_read_blocking(&i2c, device->_i2c_address, data, sizeof(data), false);
+
+    uint16_t data_bits = 0;
+    data_bits = (data[0] & 0x0F) << 8 | data[1]; // 0x0F is to mask the 4 most signifigant bits
+
+    return data_bits;
+}
+
+float ADC_bits_to_volt(struct AD5593R *device, uint16_t data_bits)
+{
+    if (!device->_int_vref_enabled)
+        return device->_ADC_max * ((float)data_bits / 4095);
+    else if (device->_ADC_2x_mode)
+        return ((float)data_bits / 4095) * 5.0f;
+    else
+        return ((float)data_bits / 4095) * 2.5f;
+}
+
+unsigned int set_ADC_seq(struct AD5593R *device, bool *channels)
+{
+    if (!channels)
+    {
+        for (uint8_t i = 0; i < 8; i++)
+        {
+            if (device->config.ADCs[i])
+            {
+                channels[i] = true;
+            }
+        }
+    }
+
+    uint8_t adc_sequence_cmd[] = {_ADAC_ADC_SEQUENCE, 0x02, channels};
+    i2c_write_blocking(&i2c, device->_i2c_address, adc_sequence_cmd, sizeof(adc_sequence_cmd), false);
+
+    uint8_t buf[] = {_ADAC_ADC_READ};
+    i2c_write_blocking(&i2c, device->_i2c_address, buf, sizeof(buf), false);
+
+    return 1;
+}
+
+uint16_t AD5593R_read_ADC_seq_bits(struct AD5593R *device)
+{
+    uint8_t data[2];
+    i2c_read_blocking(&i2c, device->_i2c_address, data, sizeof(data), false);
+
+    uint16_t data_bits = 0;
+    data_bits = (data[0] & 0x0F) << 8 | data[1]; // 0x0F is to mask the 4 most signifigant bits
+
+    return data_bits;
+}
+
+float AD5593R_read_ADC_seq(struct AD5593R *device)
+{
+    uint8_t data[2];
+    i2c_read_blocking(&i2c, device->_i2c_address, data, sizeof(data), false);
+
+    uint16_t data_bits = 0;
+    data_bits = (data[0] & 0x0F) << 8 | data[1]; // 0x0F is to mask the 4 most signifigant bits
+
+    if (!device->_int_vref_enabled)
+        return device->_ADC_max * data_bits * (1.0F / 4095);
+    else if (device->_ADC_2x_mode)
+        return ((float)data_bits / 4095) * 5.0f;
+    else
+        return ((float)data_bits / 4095) * 2.5f;
 }
 
 void ADD593R_configure_GPI(struct AD5593R *device, int channel)
@@ -576,7 +710,7 @@ void ADD593R_configure_GPO(struct AD5593R *device, int channel)
         gpio_put(device->_a0, 1); // Set the GPIO pin high
     }
 
-    printf("Channel %d is now configured as a GPO\n", channel);
+    printf("Channel %d is now configured as a GPO.\n", channel);
 }
 
 void ADD593R_configure_GPOs(struct AD5593R *device, int *channels, int num_channels)
@@ -628,28 +762,34 @@ bool *ADD593R_read_GPIs(struct AD5593R *device)
 
 void ADD593R_write_GPOs(struct AD5593R *device, bool *values)
 {
+    uint16_t data_bits = 0;
+
     if (device->_a0 > -1)
     {
         gpio_put(device->_a0, 0); // Set the GPIO pin low
     }
-
-    uint8_t gpo_write_cmd[] = {_ADAC_GPIO_WR_DATA, 0x00, 0x00};
 
     for (size_t i = 0; i < _num_of_channels; i++)
     {
         if (device->config.GPOs[i] == 1)
         {
-            gpo_write_cmd[1] = gpo_write_cmd[1] | (values[i] << i);
+            device->values.GPO_writes[1] = values[i];
+            data_bits = data_bits & values[i];
+        }
+        else
+        {
+            data_bits <<= 1;
         }
     }
+
+    uint8_t gpo_write_cmd[] = {_ADAC_GPIO_WR_DATA, 0x00, data_bits};
 
     if (device->_a0 > -1)
     {
         gpio_put(device->_a0, 0); // Set the GPIO pin low
     }
 
-    i2c_write_blocking(&i2c, device->_i2c_address, _ADAC_GPIO_WR_DATA, sizeof(_ADAC_GPIO_WR_DATA), false);
-    i2c_write_blocking(&i2c, device->_i2c_address, 0x00, sizeof(0x00), false);
+    i2c_write_blocking(&i2c, device->_i2c_address, gpo_write_cmd, sizeof(gpo_write_cmd), false);
 
     if (device->_a0 > -1)
     {
@@ -657,11 +797,11 @@ void ADD593R_write_GPOs(struct AD5593R *device, bool *values)
     }
 }
 
-// main to perform a basic test compiling for wifi chip
+// main to perform a basic test with wifi chip fw
 int main()
 {
-    bool my_DACs[8] = {1, 1, 1, 1, 0, 0, 0, 0};
-    bool my_ADCs[8] = {0, 0, 0, 0, 1, 1, 1, 1};
+    bool hw_DACs[8] = {1, 1, 1, 1, 0, 0, 0, 0}; // for testing
+    bool hw_ADCs[8] = {0, 0, 0, 0, 1, 1, 1, 1}; // for testing
 
     struct AD5593R device;
 
@@ -673,12 +813,17 @@ int main()
         return -1;
     }
 
+    // init add5593r
     AD5593R_init(&device, -1);
-        
+    // enable internal voltage reference for add5593r
     AD5593R_enable_internal_Vref(&device);
-    
+
+    // Configure DAC channels based on the values in hw_DACs array
+    AD5593R_configure_DACs(&device, hw_DACs, _num_of_channels);
+
     while (true)
     {
+        // loop to check if program is OK
         printf("led on");
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(250);
